b7a02fb06141567aecab2f50a99fcf6ca32f1b90
felixjiricka <felixjiricka@outlook.com>
2022-02-26 20:54:28 +0100

    update page header component

--- website/src/app/core/components/header/header.component.ts
+++ website/src/app/core/components/header/header.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'aconio-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'aconio-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+    menuVisible: boolean = false;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    toggleMobile() {
+        this.menuVisible = !this.menuVisible;
+    }
 }
