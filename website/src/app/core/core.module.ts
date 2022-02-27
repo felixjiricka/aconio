44b44b7ac59d3bb26a6b1b1890de0bbcc3ed8552
felixjiricka <felixjiricka@outlook.com>
2022-02-27 22:30:43 +0100

    add cta component

--- website/src/app/core/core.module.ts
+++ website/src/app/core/core.module.ts
@@ -5,10 +5,11 @@ import {HeaderComponent} from './components/header/header.component';
 import { RouterModule } from '@angular/router';
 import { InfoCardComponent } from './components/info-card/info-card.component';
 import { AngularSvgIconModule } from 'angular-svg-icon';
+import { CtaComponent } from './components/cta/cta.component';
 
 @NgModule({
-    declarations: [HeaderComponent, FooterComponent, InfoCardComponent],
+    declarations: [HeaderComponent, FooterComponent, InfoCardComponent, CtaComponent],
     imports: [CommonModule, RouterModule, AngularSvgIconModule],
-    exports: [HeaderComponent, FooterComponent, InfoCardComponent]
+    exports: [HeaderComponent, FooterComponent, InfoCardComponent, CtaComponent]
 })
 export class CoreModule {}
